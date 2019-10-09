733051b4ed8bea73c90b2c05c3263fe8528211f8
ElC-Centro <56326683+ElC-Centro@users.noreply.github.com>
2019-10-08 21:52:48 -0300

    Add files via upload

---
+++ Magia do Toque/main1.js
@@ -0,0 +1,25 @@
+// $("#accordion").on("show.bs.collapse", function() {
+// 	$("#accordion .in").collapse("hide");
+// });
+$(".navbar-nav>li>a").on("click", function() {
+	$(".navbar-collapse").collapse("hide");
+});
+
+// The function actually applying the offset
+function offsetAnchor() {
+	if (location.hash.length !== 0) {
+		window.scrollTo(window.scrollX, window.scrollY - 100);
+	}
+}
+
+// Captures click events of all <a> elements with href starting with #
+$(document).on("click", 'a[href^="#"]', function(event) {
+	// Click events are captured before hashchanges. Timeout
+	// causes offsetAnchor to be called after the page jump.
+	window.setTimeout(function() {
+		offsetAnchor();
+	}, 0);
+});
+
+// Set the offset when entering page with hash present in the url
+window.setTimeout(offsetAnchor, 0);
